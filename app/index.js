a021289d463a35b26d06c4be6d5a1f0d1fc2e5fd
Kai Hao <kevin830726@gmail.com>
2019-03-27 14:14:56 +0800

    add centralize error handling

--- app/index.js
+++ app/index.js
@@ -7,6 +7,20 @@ const { PORT } = require('./constants');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
+app.on('error', (err) => {
+  console.log(err);
+});
+
 app.use(bodyParser());
 
 app.use(dailylunch.routes()).use(dailylunch.allowedMethods());
